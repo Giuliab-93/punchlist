79974021f5de7231b9d836c06c2c8837aa80dee7
Giuliab-93 <giulia.bracci4@gmail.com>
2026-09-03 07:52:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "punchlist-v13";
+const CACHE = "punchlist-v14";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
